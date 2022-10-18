eb0e3ad9f5e942c384958d0033b70819077d938c
Anderson Luís Furlan <alsfurlan@gmail.com>
2022-10-18 19:41:09 -0300

    Atualizar data e hora em tempo real

--- src/app/data-hora/data-hora.component.ts
+++ src/app/data-hora/data-hora.component.ts
@@ -6,20 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-hora.component.css']
 })
 export class DataHoraComponent implements OnInit {
-
-  dataHora: Date;
+  timer: any;
+  
+  dataHora: Date = new Date();
   // dataHoraString: string;
   valor: number;
   // valor: string;
 
   constructor() {
     // this.dataHoraString = new Date().toLocaleString();
-    this.dataHora = new Date();
+    this.startTimer();
     // this.valor = new Intl.NumberFormat('pt-BR').format(2.54);
     this.valor = 2.54;
   }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleTimer() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = undefined;
+    } else {
+      this.startTimer();
+    }
   }
-
+  private startTimer() {
+    this.timer = setInterval(() => {
+      this.dataHora = new Date();
+    }, 1000);
+  }
 }
