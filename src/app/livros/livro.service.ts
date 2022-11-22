772e5ae151ae7786616f5f5efcdc58604c616019
Anderson Luís Furlan <alsfurlan@gmail.com>
2022-11-22 19:57:40 -0300

    Uso de variável do environment

--- src/app/livros/livro.service.ts
+++ src/app/livros/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Livro } from './livro.interface';
 
 @Injectable({
@@ -10,23 +11,23 @@ export class LivroService {
   constructor(private http: HttpClient) { }
 
   getLivro(id: number): Observable<Livro> {
-    return this.http.get<Livro>(`http://localhost:3000/livros/${id}`);
+    return this.http.get<Livro>(`${environment.apiUrl}/livros/${id}`);
   }
 
   getLivros(): Observable<Livro[]> {
-    return this.http.get<Livro[]>('http://localhost:3000/livros');
+    return this.http.get<Livro[]>(`${environment.apiUrl}/livros`);
   }
 
   save(livro: Livro): Observable<Livro> {
-    return this.http.post<Livro>('http://localhost:3000/livros', livro);
+    return this.http.post<Livro>(`${environment.apiUrl}/livros`, livro);
   }
 
-  
+
   update(livro: Livro): Observable<Livro> {
-    return this.http.put<Livro>(`http://localhost:3000/livros/${livro.id}`, livro);
+    return this.http.put<Livro>(`${environment.apiUrl}/livros/${livro.id}`, livro);
   }
 
   remove({ id }: Livro): Observable<void> {
-    return this.http.delete<void>(`http://localhost:3000/livros/${id}`);
+    return this.http.delete<void>(`${environment.apiUrl}/livros/${id}`);
   }
 }
